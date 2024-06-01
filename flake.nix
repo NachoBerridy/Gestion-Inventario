466e5801f1b458ea73633d071be41a7f7dfebc19
Facundo Lucero <facundon.lucero@alumnos.frm.utn.edu.ar>
2024-06-01 17:43:01 -0300

    feat(db): se crea script para migracion

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    systems.url = "github:nix-systems/default";
+    nix-vscode-extensions.url = "github:nix-community/nix-vscode-extensions";
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , systems
+    , nix-vscode-extensions
+    , ...
+    } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = import nixpkgs {
+              system = system;
+              config.allowUnfree = true;
+            };
+            extensions = nix-vscode-extensions.extensions.${system};
+            codeWithExtensions =
+              let
+                inherit (pkgs) vscode-with-extensions;
+                someExtensions = with (extensions.forVSCodeVersion pkgs.vscode.version).vscode-marketplace; [
+                  dbaeumer.vscode-eslint
+                  usernamehw.errorlens
+                  ms-vsliveshare.vsliveshare
+                ];
+              in
+              (vscode-with-extensions.override {
+                vscodeExtensions = someExtensions;
+              });
+          in
+          {
+            default = pkgs.mkShell {
+
+              NIX_PATH = "nixpkgs=" + pkgs.path;
+
+              buildInputs = with pkgs;[
+                nodejs_20
+                sqlite
+              ] ++ [ codeWithExtensions ];
+
+            };
+          });
+
+    };
+
+}
