7dc3128cbd33378c1f82467091c44362c598bc74
Facundo Lucero <facundon.lucero@alumnos.frm.utn.edu.ar>
2024-06-05 23:04:56 -0300

    feat: se agrega util para obtener la conexion a la DB

---
+++ utils/getDb.ts
@@ -0,0 +1,21 @@
+import { open, Database } from "sqlite";
+import sqlite3 from "sqlite3";
+
+let currentDb: Database<sqlite3.Database, sqlite3.Statement> | null = null;
+
+export default async function getDb(): Promise<
+  Database<sqlite3.Database, sqlite3.Statement>
+> {
+  if (currentDb !== null) {
+    return currentDb;
+  }
+
+  const db = await open({
+    filename: "./db/test.db",
+    driver: sqlite3.Database,
+  });
+
+  currentDb = db;
+
+  return db;
+}
