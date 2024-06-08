a15c648bcc9ac5029c17232fc2fe057cca214282
Facundo Lucero <facundon.lucero@alumnos.frm.utn.edu.ar>
2024-06-08 12:45:20 -0300

    feat(proveedores): paginacion

--- pages/api/proveedores/index.ts
+++ pages/api/proveedores/index.ts
@@ -1,7 +1,5 @@
 import getDbConnection from "@/utils/getDb";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { open, Database } from "sqlite";
-import sqlite3 from "sqlite3";
 
 // model
 export interface IProveedor {
@@ -13,19 +11,29 @@ export interface IProveedor {
 }
 
 // querys
-
 async function getProveedores(
-  lastId: number = 0,
-  offset: number = 10
-): Promise<IProveedor[]> {
+  limit: number = 10,
+  offset: number = 0
+): Promise<{
+  rows: IProveedor[];
+  totalRows: number;
+}> {
   const db = await getDbConnection();
 
-  const result = await db.all<IProveedor[]>(
-    "select * from Proveedor where id > ?1 order by id limit ?2",
-    [lastId, offset]
+  const totalRows =
+    (
+      await db.get<{ "count(id)": number }>("select count(id) from proveedor")
+    )?.["count(id)"] ?? 0;
+
+  const rows = await db.all<IProveedor[]>(
+    "select * from Proveedor limit ?1 offset ?2",
+    [limit, offset]
   );
 
-  return result;
+  return {
+    rows,
+    totalRows,
+  };
 }
 
 export default async function handler(
@@ -33,15 +41,23 @@ export default async function handler(
   res: NextApiResponse
 ) {
   if (req.method === "GET") {
-    const { lastId, pageSize } = req.query;
-    const parsedLastId = Number(lastId);
+    const { pageSize, offset } = req.query;
+    const parsedOffset = Number(offset);
     const parsedPageSize = Number(pageSize);
 
-    console.log(parsedLastId, parsedPageSize);
+    const LIMIT_PAGESIZE = 10;
+
+    if (parsedPageSize > LIMIT_PAGESIZE) {
+      res
+        .status(400)
+        .json(
+          `El parametro page size supera el límite permitido ${LIMIT_PAGESIZE}`
+        );
+    }
 
     const proveedores = await getProveedores(
-      Number.isNaN(parsedLastId) ? undefined : parsedLastId,
-      Number.isNaN(parsedPageSize) ? undefined : parsedPageSize
+      Number.isNaN(parsedPageSize) ? undefined : parsedPageSize,
+      Number.isNaN(parsedOffset) ? undefined : parsedOffset
     );
 
     res.status(200).json(proveedores);
--- pages/proveedores/index.tsx
+++ pages/proveedores/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import useSWR from "swr";
 import { IProveedor } from "../api/proveedores";
 import React from "react";
@@ -44,28 +45,56 @@ function Proveedor({ proveedor }: { proveedor: IProveedor }) {
 }
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const PAGE_SIZE = 5;
 
 export default function Proveedores() {
-  const { data, error, isLoading } = useSWR<IProveedor[]>(
-    "/api/proveedores?pageSize=5",
+  const [currentPage, setPage] = useState<number>(1);
+
+  const { data, error, isLoading } = useSWR<{
+    rows: IProveedor[];
+    totalRows: number;
+  }>(
+    `/api/proveedores?pageSize=${PAGE_SIZE}&offset=${
+      PAGE_SIZE * (currentPage - 1)
+    }`,
     fetcher
   );
 
-  if (error) return <div>Error al cargar los proveedores</div>;
+  if (error || undefined === data)
+    return <div>Error al cargar los proveedores</div>;
   if (isLoading) return <div>Cargando...</div>;
 
-  // render data
+  const totalPages = Math.ceil(data?.totalRows / PAGE_SIZE);
+
+  function handlePageButton(action: "inc" | "dec") {
+    if (currentPage === 1 && action === "dec") return;
+    if (currentPage === totalPages && action === "inc") return;
+
+    setPage((prev) => {
+      if (action === "inc") return prev + 1;
+      return prev - 1;
+    });
+  }
+
   return (
     <div className="p-10 w-full h-screen flex flex-col gap-3">
-      <div className="text-center">PROVEEDORES</div>
+      <div className="text-center">PROVEEDORES {totalPages}</div>
       <div className="flex gap-3 flex-col p-1">
         <input></input>
         <div>SELECTOR</div>
       </div>
       <div className="flex flex-col gap-10">
-        {data?.map((p) => (
+        {data?.rows.map((p) => (
           <Proveedor key={p.id} proveedor={p} />
         ))}
+      </div>
+
+      <div className="w-full flex justify-end gap-3">
+        <button onClick={() => handlePageButton("dec")}>Prev</button>
+        <span>
+          {currentPage} / {totalPages}
+        </span>
+        <button onClick={() => handlePageButton("inc")}>Next</button>
       </div>
     </div>
   );
