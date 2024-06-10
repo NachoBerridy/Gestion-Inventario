01af767cc9d9c8f30165c27a817540577f2381af
fyanzon <facundo.yanzon@alumnos.frm.utn.edu.ar>
2024-06-10 16:40:44 -0300

    Landing Page

---
+++ pages/LandingPage.tsx
@@ -0,0 +1,113 @@
+import Link from "next/link";
+export default function LandingPage() {
+  interface IPage {
+    name: string;
+    path: string;
+    iconPath: string;
+  }
+  const pages: IPage[] = [
+    {
+      name: "Modelos de Inventario",
+      path: "/proveedores",
+      iconPath:
+        "m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z",
+    },
+    {
+      name: "Proveedores",
+      path: "/proveedores",
+      iconPath:
+        "M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12",
+    },
+    {
+      name: "Articulos",
+      path: "/articulos",
+      iconPath:
+        "M13.5 21v-7.5a.75.75 0 0 1 .75-.75h3a.75.75 0 0 1 .75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349M3.75 21V9.349m0 0a3.001 3.001 0 0 0 3.75-.615A2.993 2.993 0 0 0 9.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 0 0 2.25 1.016c.896 0 1.7-.393 2.25-1.015a3.001 3.001 0 0 0 3.75.614m-16.5 0a3.004 3.004 0 0 1-.621-4.72l1.189-1.19A1.5 1.5 0 0 1 5.378 3h13.243a1.5 1.5 0 0 1 1.06.44l1.19 1.189a3 3 0 0 1-.621 4.72M6.75 18h3.75a.75.75 0 0 0 .75-.75V13.5a.75.75 0 0 0-.75-.75H6.75a.75.75 0 0 0-.75.75v3.75c0 .414.336.75.75.75Z",
+    },
+    {
+      name: "Orden de compra",
+      path: "/proveedores",
+      iconPath:
+        "M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z",
+    },
+    {
+      name: "Predicción de demanda",
+      path: "/proveedores",
+      iconPath:
+        "M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25M9 16.5v.75m3-3v3M15 12v5.25m-4.5-15H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z",
+    },
+    {
+      name: "Venta",
+      path: "/proveedores",
+      iconPath:
+        "m9 14.25 6-6m4.5-3.493V21.75l-3.75-1.5-3.75 1.5-3.75-1.5-3.75 1.5V4.757c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0c1.1.128 1.907 1.077 1.907 2.185ZM9.75 9h.008v.008H9.75V9Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm4.125 4.5h.008v.008h-.008V13.5Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z",
+    },
+  ];
+
+  return (
+    <div
+      style={{
+        width: "100%",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <div
+        style={{
+          width: "80%",
+          height: "80%",
+          display: "grid",
+          gridTemplateColumns: "repeat(3, calc(100%/3))",
+          gridTemplateRows: "repeat(2, 50%)",
+          alignItems: "center",
+          justifyItems: "center",
+        }}
+      >
+        {pages.map((page) => (
+          <Link
+            href={page.path}
+            style={{
+              width: "70%",
+              height: "70%",
+              display: "flex",
+              borderRadius: "10%",
+              alignItems: "center",
+              justifyContent: "center",
+              backgroundColor: "#ebebeb",
+              boxShadow: "10px 10px 5px 0px rgba(0,0,0,0.32)",
+              cursor: "pointer",
+              flexDirection: "column",
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="size-12"
+              style={{ width: "80px", height: "80px" }}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d={page.iconPath}
+              />
+            </svg>
+            <span
+              style={{
+                marginTop: "5px",
+                fontSize: "24px",
+                fontWeight: "bold",
+              }}
+            >
+              {page.name}
+            </span>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
